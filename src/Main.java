1915b7ad42d44ea1dd29c8def9f67b7135ec7721
Arindam Saha <asaha15071998@gmail.com>
2023-02-21 08:21:34 +0530

    Creating The Board Using OOPS Concept

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+interface TicTacToe{
+    static char[] board =new char[10];
+    public void board();
+}
+
+class TicTacToeService implements TicTacToe {
+
+    @Override
+    public void board() {
+        for (int i = 1; i <= 9; i++) {
+            board[i] = ' ';
+        }
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
 
